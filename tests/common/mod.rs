d505e291d35c4bbec09c85759df685466805ff85
Ceri Storey <cez@necrofish.org.uk>
2015-06-01 21:39:54 +0100

    Expand, and seperate quickcheck test.

---
+++ tests/common/mod.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+use yak_client::Client;
+
+pub fn open_from_env(env_var: &str, name: &str) -> Client {
+  let yak_url = env::var(env_var).ok()
+    .expect(format!("env var {} not found", env_var).as_str());
+  let full_url = format!("{}-{}", yak_url, name);
+  Client::connect(&full_url).unwrap()
+}
+
+pub fn open_client(name: &str) -> (Client, Client) {
+  let head = open_from_env("YAK_HEAD", name);
+  let tail = open_from_env("YAK_TAIL", name);
+  (head, tail)
+}
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,31 +1,12 @@
 #![feature(convert)]
-#![feature(plugin)]
-#![plugin(quickcheck_macros)]
 
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 extern crate yak_client;
 
-extern crate quickcheck;
-
-use quickcheck::quickcheck;
-use std::env;
-
-use yak_client::{Client, YakError};
-
-fn open_from_env(env_var: &str, name: &str) -> Client {
-  let yak_url = env::var(env_var).ok()
-    .expect(format!("env var {} not found", env_var).as_str());
-  let full_url = format!("{}-{}", yak_url, name);
-  Client::connect(&full_url).unwrap()
-}
-
-fn open_client(name: &str) -> (Client, Client) {
-  let head = open_from_env("YAK_HEAD", name);
-  let tail = open_from_env("YAK_TAIL", name);
-  (head, tail)
-}
+mod common;
+use common::*;
 
 #[test]
 fn test_put_read_empty() {
@@ -87,15 +68,4 @@ fn test_truncate() {
 
   let resp = tail.read(key.as_bytes()).unwrap();
   assert_eq!(resp.len(), 0);
-}
-
-#[quickcheck]
-fn test_put_read_single_value_qc(key: Vec<u8>, val: Vec<u8>) -> Result<bool, YakError> {
-  let (mut head, mut tail) = open_client("test_put_read_single_value_qc");
-  try!(head.truncate());
-  try!(head.write(&key, &val));
-
-  let resp = try!(tail.read(&key));
-  let contents : Vec<_> = resp.iter().map(|x| x.content.to_vec()).collect();
-  Ok(contents == vec![val])
 }
